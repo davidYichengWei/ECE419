86d7bb0605a5045c8264653fe3d146b2902588c8
Lepabryce <lepasanabryce@gmail.com>
2023-01-22 23:19:59 -0500

    Add Message class

---
+++ M1/src/shared/messages/Message.java
@@ -0,0 +1,30 @@
+package shared.messages;
+
+import java.io.Serializable;
+
+public class Message implements KVMessage, Serializable {
+    private String key;
+    private String value;
+    private StatusType status;
+
+    Message(String key, String value, StatusType status) {
+        this.key = key;
+        this.value = value;
+        this.status = status;
+    }
+
+    @Override
+    public String getKey() {
+        return key;
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public StatusType getStatus() {
+        return status;
+    }
+}
