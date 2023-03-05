370b116caa3f765f91bdbcc2c2101fa93ae24767
Gengyang Wang <gengyang.wang@mail.utoronto.ca>
2023-03-05 18:52:42 -0500

    test email extraction

--- src/testing/PerformanceTest.java
+++ src/testing/PerformanceTest.java
@@ -1,145 +1,153 @@
 package testing;
-
+import app_kvClient.KVClient;
 import app_kvServer.KVServer;
 import java.io.*;
 import java.util.*;
 public class PerformanceTest {
-    private KVServer server;
+    private KVServer[] server_list;
 
     public PerformanceTest() {
         try {
-            this.server = new KVServer("127.0.0.1", 8080, 2000, "LRU", "db_files");
+            // this.server = new KVServer("127.0.0.1", 8080, 2000, "LRU", "db_files");
+            this.server_list = new KVServer[5];
+            this.server_list[0] = new KVServer("127.0.0.1", 10000, 2000, "LRU", "db_files");
+            
+            this.server_list[1] = new KVServer("127.0.0.1", 10001, 2000, "LRU", "db_files");
+            this.server_list[2] = new KVServer("127.0.0.1", 10002, 2000, "LRU", "db_files");
+            this.server_list[3] = new KVServer("127.0.0.1", 10003, 2000, "LRU", "db_files");
+            this.server_list[4] = new KVServer("127.0.0.1", 10004, 2000, "LRU", "db_files");
+            
         } catch (Exception e) {
             
         }
     }
-    public void performancePutShort(int numGet, int numPut){
-        long  startTime = System.currentTimeMillis(); 
-        try {
-            for(int i=0;i<numPut;i++){
-                server.putKV(Integer.toString(i), Integer.toString(i));
-            }
+    // public void performancePutShort(int numGet, int numPut){
+    //     long  startTime = System.currentTimeMillis(); 
+    //     try {
+    //         for(int i=0;i<numPut;i++){
+    //             server.putKV(Integer.toString(i), Integer.toString(i));
+    //         }
             
-        } catch (Exception e) {
+    //     } catch (Exception e) {
             
-        }
+    //     }
         
 
-        long endTime = System.currentTimeMillis();
+    //     long endTime = System.currentTimeMillis();
 
-        System.out.println("Number of short puts: "+ Integer.toString(numPut)+ "_____Running time for this test is " + (endTime - startTime) + "ms"); 
-    }
-    public void performancePutMedium(int numGet, int numPut){
-        long  startTime = System.currentTimeMillis(); 
-        try {
-            for(int i=0;i<numPut;i++){
-                server.putKV(Integer.toString(i), "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO");
-            }
+    //     System.out.println("Number of short puts: "+ Integer.toString(numPut)+ "_____Running time for this test is " + (endTime - startTime) + "ms"); 
+    // }
+    // public void performancePutMedium(int numGet, int numPut){
+    //     long  startTime = System.currentTimeMillis(); 
+    //     try {
+    //         for(int i=0;i<numPut;i++){
+    //             server.putKV(Integer.toString(i), "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO");
+    //         }
             
-        } catch (Exception e) {
+    //     } catch (Exception e) {
             
-        }
+    //     }
         
 
-        long endTime = System.currentTimeMillis();
+    //     long endTime = System.currentTimeMillis();
 
-        System.out.println("Number of short puts: "+ Integer.toString(numPut)+ "_____Running time for this test is " + (endTime - startTime) + "ms"); 
-    }
-    public void performancePutLarge(int numGet, int numPut){
-        long  startTime = System.currentTimeMillis(); 
-        try {
-            for(int i=0;i<numPut;i++){
-                server.putKV(Integer.toString(i), "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLO");
-            }
+    //     System.out.println("Number of short puts: "+ Integer.toString(numPut)+ "_____Running time for this test is " + (endTime - startTime) + "ms"); 
+    // }
+    // public void performancePutLarge(int numGet, int numPut){
+    //     long  startTime = System.currentTimeMillis(); 
+    //     try {
+    //         for(int i=0;i<numPut;i++){
+    //             server.putKV(Integer.toString(i), "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLO");
+    //         }
             
-        } catch (Exception e) {
+    //     } catch (Exception e) {
             
-        }
+    //     }
         
 
-        long endTime = System.currentTimeMillis();
+    //     long endTime = System.currentTimeMillis();
 
-        System.out.println("Number of large puts: "+ Integer.toString(numPut)+ "_____Running time for this test is " + (endTime - startTime) + "ms"); 
-    }
-    public void performance80Put20Get(int numGet, int numPut){
-        long  startTime = System.currentTimeMillis(); 
-        try {
-            for(int i=0;i<numPut;i++){
-                // server.putKV(Integer.toString(i), Integer.toString(i));
+    //     System.out.println("Number of large puts: "+ Integer.toString(numPut)+ "_____Running time for this test is " + (endTime - startTime) + "ms"); 
+    // }
+    // public void performance80Put20Get(int numGet, int numPut){
+    //     long  startTime = System.currentTimeMillis(); 
+    //     try {
+    //         for(int i=0;i<numPut;i++){
+    //             // server.putKV(Integer.toString(i), Integer.toString(i));
 
-                // server.putKV(Integer.toString(i), "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO");
+    //             // server.putKV(Integer.toString(i), "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO");
 
-                server.putKV(Integer.toString(i), "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLO");
-            }
+    //             server.putKV(Integer.toString(i), "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLO");
+    //         }
 
-            for(int i=0;i<numGet;i++){
-                server.getKV(Integer.toString(i));
-            }
-        } catch (Exception e) {
+    //         for(int i=0;i<numGet;i++){
+    //             server.getKV(Integer.toString(i));
+    //         }
+    //     } catch (Exception e) {
             
-        }
+    //     }
         
 
-        long endTime = System.currentTimeMillis();
+    //     long endTime = System.currentTimeMillis();
 
-        System.out.println("Number of short puts: "+ Integer.toString(numPut)+ "_____Running time for this test is " + (endTime - startTime) + "ms"); 
-    }
-    public void performance20Put80Get(int numGet, int numPut){
-        long  startTime = System.currentTimeMillis(); 
-        try {
-            for(int i=0;i<numPut;i++){
-                // server.putKV(Integer.toString(i), Integer.toString(i));
+    //     System.out.println("Number of short puts: "+ Integer.toString(numPut)+ "_____Running time for this test is " + (endTime - startTime) + "ms"); 
+    // }
+    // public void performance20Put80Get(int numGet, int numPut){
+    //     long  startTime = System.currentTimeMillis(); 
+    //     try {
+    //         for(int i=0;i<numPut;i++){
+    //             // server.putKV(Integer.toString(i), Integer.toString(i));
 
-                // server.putKV(Integer.toString(i), "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO");
+    //             // server.putKV(Integer.toString(i), "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO");
 
-                server.putKV(Integer.toString(i), "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
-                "HELLOHELLOHELLOHELLOHELLOHELLOHELLO");
-            }
+    //             server.putKV(Integer.toString(i), "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO"+
+    //             "HELLOHELLOHELLOHELLOHELLOHELLOHELLO");
+    //         }
 
-            for(int i=0;i<8;i++){
-                for(int j=0;j<100;j++){
-                    server.getKV(Integer.toString(j));
-                }
+    //         for(int i=0;i<8;i++){
+    //             for(int j=0;j<100;j++){
+    //                 server.getKV(Integer.toString(j));
+    //             }
                 
-            }
-        } catch (Exception e) {
+    //         }
+    //     } catch (Exception e) {
             
-        }
+    //     }
         
 
-        long endTime = System.currentTimeMillis();
+    //     long endTime = System.currentTimeMillis();
 
-        System.out.println("Number of short puts: "+ Integer.toString(numPut)+ "_____Running time for this test is " + (endTime - startTime) + "ms"); 
-    }
+    //     System.out.println("Number of short puts: "+ Integer.toString(numPut)+ "_____Running time for this test is " + (endTime - startTime) + "ms"); 
+    // }
     public void scan_folder(String path){
         File f = new File("/Users/beale/Desktop/maildir/allen-p/sent"); 
         Map<String, String> pairs = new HashMap<String, String>();
@@ -162,6 +170,12 @@ public class PerformanceTest {
 
         catch(Exception e){
         }
+    }
+    public void run_tests(){
+        long  startTime = System.currentTimeMillis(); 
+        
+
+        long endTime = System.currentTimeMillis();
     }
     public static void main(String[] args) {
         PerformanceTest p =  new PerformanceTest();
