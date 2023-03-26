4b2916048c3d7104856ab991901631255dfa8fe5
Gengyang Wang <gengyang.wang@mail.utoronto.ca>
2023-03-26 19:45:29 -0400

    perf test

--- src/testing/PerformanceTest.java
+++ src/testing/PerformanceTest.java
@@ -4,26 +4,20 @@ import app_kvServer.KVServer;
 import java.io.*;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
+import client.KVStore;
+import shared.messages.KVMessage;
+import shared.messages.KVMessage.StatusType;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 public class PerformanceTest {
-    private KVServer[] server_list;
-
+    private KVStore kvClient;
+    Map<String, String> test_pairs;
+    private static Logger logger = Logger.getRootLogger();
     public PerformanceTest() {
-        try {
-            long  startTime = System.currentTimeMillis(); 
         
-            int i=0;
-            // this.server = new KVServer("127.0.0.1", 8080, 2000, "LRU", "db_files");
-            this.server_list = new KVServer[10];
-            for(i=0;i<5;i++){
-                this.server_list[i] = new KVServer("127.0.0.1", 10200+i, "localhost", 2000, "LRU", "db_files");
-                // TimeUnit.SECONDS.sleep(1);
-            }
-            long endTime = System.currentTimeMillis();
-        long res = endTime-startTime;
-        System.out.println("time to create" +i+" servers:________________________________"+ res);
-        } catch (Exception e) {
-            
-        }
     }
     // public void performancePutShort(int numGet, int numPut){
     //     long  startTime = System.currentTimeMillis(); 
@@ -168,25 +162,139 @@ public class PerformanceTest {
                     one_file+=stringLine;
                 }
                 pairs.put(file.getName(), one_file);
+                
                 // System.out.println(one_file);
             }
+            this.test_pairs = pairs;
+            
         }
 
         catch(Exception e){
         }
     }
     public void run_tests(){
-        long  startTime = System.currentTimeMillis(); 
-        
-        long endTime = System.currentTimeMillis();
+        try {
+            
+            
+            
+            // Start 2 new servers
+
+            KVServer server2 = new KVServer("localhost", 50001, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server3 = new KVServer("localhost", 50002, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server4 = new KVServer("localhost", 50003, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server5 = new KVServer("localhost", 50004, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server6 = new KVServer("localhost", 50005, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server7 = new KVServer("localhost", 50006, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server8 = new KVServer("localhost", 50007, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server9 = new KVServer("localhost", 50008, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server10 = new KVServer("localhost", 50009, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server11 = new KVServer("localhost", 50010, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server12 = new KVServer("localhost", 50011, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server13 = new KVServer("localhost", 50012, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server14 = new KVServer("localhost", 50013, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server15 = new KVServer("localhost", 50014, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server16 = new KVServer("localhost", 50015, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server17 = new KVServer("localhost", 50016, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server18 = new KVServer("localhost", 50017, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server19 = new KVServer("localhost", 50018, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server20 = new KVServer("localhost", 50019, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            KVServer server21 = new KVServer("localhost", 50020, "localhost", 10, "FIFO", "db_files");
+            Thread.sleep(1000);
+            // Wait for 5 seconds to allow the server to start
+            
+            
+            long  startTime = System.currentTimeMillis();
+            kvClient = new KVStore("localhost", 50001);
+            kvClient.connect();
+            int num=0;
+            for(String i:this.test_pairs.keySet()){
+                // System.out.println(i);
+                kvClient.put(i, test_pairs.get(i));
+                num++;
+                // if(num==100){
+                //     break;
+                // }
+            }
+            // for(int i=0;i<20;i++){
+            //     kvClient.get("12.");
+            // }
+            long  endTime = System.currentTimeMillis(); 
+            kvClient.disconnect();
+            long res = endTime-startTime;
+            System.out.println("___________________time to put requests:________________________________-4000"+ res);
+            server2.clearCache();
+            server2.clearStorage();
+            server3.clearCache();
+            server3.clearStorage();
+            server4.clearCache();
+            server4.clearStorage();
+            server5.clearCache();
+            server5.clearStorage();
+            server6.clearCache();
+            server6.clearStorage();
+            server7.clearCache();
+            server7.clearStorage();
+            server8.clearCache();
+            server8.clearStorage();
+            server9.clearCache();
+            server9.clearStorage();
+            server10.clearCache();
+            server10.clearStorage();
+            server11.clearCache();
+            server11.clearStorage();
+            server12.clearCache();
+            server12.clearStorage();
+            server13.clearCache();
+            server13.clearStorage();
+            server14.clearCache();
+            server14.clearStorage();
+            server15.clearCache();
+            server15.clearStorage();
+            server16.clearCache();
+            server16.clearStorage();
+            server17.clearCache();
+            server17.clearStorage();
+            server18.clearCache();
+            server18.clearStorage();
+            server19.clearCache();
+            server19.clearStorage();
+            server20.clearCache();
+            server20.clearStorage();
+            server21.clearCache();
+            server21.clearStorage();
+        } catch (Exception e) {
+            
+        }
     }
+
     public static void main(String[] args) {
-        long  startTime = System.currentTimeMillis(); 
+        // long  startTime = System.currentTimeMillis(); 
         PerformanceTest p =  new PerformanceTest();
-        p=null;
-        long endTime = System.currentTimeMillis();
-        long res = endTime-startTime;
-        System.out.println("time to create 5 servers:"+ res);
+        p.scan_folder("");
+        p.run_tests();
+        
+        // long endTime = System.currentTimeMillis();
+        // long res = endTime-startTime;
+        // System.out.println("time to create 5 servers:"+ res);
         
         // p.performancePutShort(0, 1000);
     //     p.server.clearStorage();
@@ -195,6 +303,6 @@ public class PerformanceTest {
         // p.performance20Put80Get(800, 200);
 
         // p.server.clearStorage();
-        // p.scan_folder("");
+        
     }
 }
